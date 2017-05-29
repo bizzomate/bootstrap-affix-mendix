7c128877df5eac866d8334f8b5fb9565784ec6fa
Jelle Dekker <jelle.dekker@bizzomate.com>
2017-05-29 15:41:34 +0200

    Update for Mx7
    Update in the way bootstrap-affix is loaded for compatibility with Mx7

--- src/BootstrapAffix/widget/BootstrapAffix.js
+++ src/BootstrapAffix/widget/BootstrapAffix.js
@@ -36,10 +36,11 @@ define([
 
     "BootstrapAffix/lib/jquery-1.11.2",
     "BootstrapAffix/lib/bootstrap-affix-3.3.7",
-], function(declare, _WidgetBase, _TemplatedMixin, dom, dojoDom, dojoProp, dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, dojoLang, dojoText, dojoHtml, dojoEvent, _jQuery, BootstrapAffix) {
+], function (declare, _WidgetBase, _TemplatedMixin, dom, dojoDom, dojoProp, dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, dojoLang, dojoText, dojoHtml, dojoEvent, _jQuery, BootstrapAffix) {
     "use strict";
 
     var $ = _jQuery.noConflict(true);
+    var j$ = BootstrapAffix.createInstance($);
 
     // Declare widget's prototype.
     return declare("BootstrapAffix.widget.BootstrapAffix", [_WidgetBase], {
@@ -49,33 +50,33 @@ define([
         offsetTop: "",
 
         // dojo.declare.constructor is called to construct the widget instance. Implement to initialize non-primitive properties.
-        constructor: function() {
+        constructor: function () {
             //logger.level(logger.DEBUG);
             logger.debug(this.id + ".constructor");
         },
 
         // mxui.widget._WidgetBase.update is called when context is changed or initialized. Implement to re-render and / or fetch data.
-        update: function(obj, callback) {
+        update: function (obj, callback) {
             logger.debug(this.id + ".update");
 
             this._executeCallback(callback, "update");
         },
 
         // mxui.widget._WidgetBase.resize is called when the page's layout is recalculated. Implement to do sizing calculations. Prefer using CSS instead.
-        resize: function(box) {
+        resize: function (box) {
             logger.debug(this.id + ".resize");
 
             if ($(this.itemSelector).outerHeight() + this.offsetTop >= $(window).height()) {
                 $(this.itemSelector).removeClass('affix');
             } else {
-                $(this.itemSelector).affix({
+                j$(this.itemSelector).affix({
                     offset: this.offsetTop
                 });
-                $(this.itemSelector).affix('checkPosition');
+                j$(this.itemSelector).affix('checkPosition');
             }
         },
 
-        _executeCallback: function(cb, from) {
+        _executeCallback: function (cb, from) {
             logger.debug(this.id + "._executeCallback" + (from ? " from " + from : ""));
             if (cb && typeof cb === "function") {
                 cb();
